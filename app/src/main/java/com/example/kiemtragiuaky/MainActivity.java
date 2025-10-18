09f6b1b92766f0b7d73bb79e4a5a448ad65302ab
Triều Tiên <23115053122336@sv.ute.udn.vn>
2025-10-18 09:23:40 +0700

    nộp

--- app/src/main/java/com/example/kiemtragiuaky/MainActivity.java
+++ app/src/main/java/com/example/kiemtragiuaky/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.kiemtragiuaky;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +16,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txtHoTen, txtEmail, txtSdt;
+    RadioGroup radioGioiTinh;
+    Button btnLuu,btnLamMoi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,53 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        txtHoTen=findViewById(R.id.txtHoTen);
+        txtEmail=findViewById(R.id.txtEmail);
+        txtSdt=findViewById(R.id.txtsdt);
+        radioGioiTinh=findViewById(R.id.radioGioiTinh);
+        btnLuu=findViewById(R.id.btnLuu);
+        btnLamMoi=findViewById(R.id.btnLamMoi);
+        btnLamMoi.setOnClickListener(v -> {
+            txtHoTen.setText("");
+            txtEmail.setText("");
+            txtSdt.setText("");
+            radioGioiTinh.clearCheck();
+        });
+        btnLuu.setOnClickListener(v -> {
+            String hoTen=txtHoTen.getText().toString().trim();
+            String email=txtEmail.getText().toString().trim();
+            String sdt=txtSdt.getText().toString().trim();
+            if (hoTen.isEmpty())
+            {
+                Toast.makeText(this,"Vui lòng nhập Họ và tên",Toast.LENGTH_SHORT).show();
+                txtHoTen.requestFocus();
+                return;
+            }
+            if (sdt.isEmpty())
+            {
+                Toast.makeText(this,"Vui lòng nhập Số điện thoa",Toast.LENGTH_SHORT).show();
+                txtSdt.requestFocus();
+                return;
+            }
+            String gioiTinh="";
+            int chon= radioGioiTinh.getCheckedRadioButtonId();
+            if(chon !=-1)
+            {
+                RadioButton nutChon=findViewById(chon);
+                gioiTinh=nutChon.getText().toString();
+            }
+            else
+            {
+                Toast.makeText(this,"Vui lòng chọn giới tinnh",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent intent=new Intent(MainActivity.this,Profile.class);
+            intent.putExtra("hoTen",hoTen);
+            intent.putExtra("email",email);
+            intent.putExtra("sdt",sdt);
+            intent.putExtra("gioiTinh",gioiTinh);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/kiemtragiuaky/Profile.java
+++ app/src/main/java/com/example/kiemtragiuaky/Profile.java
@@ -1,6 +1,9 @@
 package com.example.kiemtragiuaky;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
-
+    TextView txtHoTen,txtEmail,txtSDT,txtGioiTinh;
+    ImageView btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,22 @@ public class Profile extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        txtHoTen=findViewById(R.id.txtHoTen);
+        txtEmail=findViewById(R.id.txtEmail);
+        txtSDT=findViewById(R.id.txtsdt);
+        txtGioiTinh=findViewById(R.id.txtGioiTinh);
+        Intent intent=getIntent();
+        String hoTen=intent.getStringExtra("hoTen");
+        String email=intent.getStringExtra("email");
+        String sdt=intent.getStringExtra("sdt");
+        String gioiTinh=intent.getStringExtra(("gioiTinh"));
+        txtHoTen.setText("Họ và tên: "+hoTen);
+        txtEmail.setText("Email: "+email);
+        txtSDT.setText("sdt"+sdt);
+        txtGioiTinh.setText("gioiTinh"+gioiTinh);
+        btnBack.setOnClickListener(v -> {
+            finish();
         });
     }
 }
